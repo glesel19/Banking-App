65c8832b1a26d335a9af1b6369f2d0cfc61b3378
EneHax20 <EneHax20@htl-shkoder.com>
2025-05-27 12:30:46 +0200

    Auth von enes

--- src/main/java/com/example/bankingapp/services/AuthService.java
+++ src/main/java/com/example/bankingapp/services/AuthService.java
@@ -5,6 +5,7 @@ import com.example.bankingapp.models.User;
 public class AuthService {
     private static User loggedInUser;
 
+
     public static boolean login(String username, String password) {
         // Mock check
         if (username.equals("admin") && password.equals("1234")) {
